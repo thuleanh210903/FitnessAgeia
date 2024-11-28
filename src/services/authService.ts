3396f01461462f4bc4a20e83c26ef8b627c89d34
ThuLe <thuanhle219@gmail.com>
2024-11-28 20:57:43 +0700

    update: Update API login trainer, add alert for empty input

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,7 +4,7 @@ const API_URL = "http://localhost:40001";
 
 export const login = async (email: string, password: string) => {
     try {
-        const response = await axios.post(`${API_URL}/auth/login`, {email,password})
+        const response = await axios.post(`${API_URL}/auth/login-trainer`, {email,password})
         return response.data;
 
     }
